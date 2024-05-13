8881fd73d72ad2df5c54c67f816697f6b6ce6d27
AmanMourya0 <118892102+AmanMourya0@users.noreply.github.com>
2024-05-13 21:32:22 +0000

    Data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const userId=3421
+let email="example@gmail.com"
+var password="23455"
+accountCity="vadodara"
+let userState;
+// var not to be used becoz of block scop and funtional scope
+console.table([userId,email,password,accountCity,userState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using node.js, not browser
+let name='aman' // string
+let age=21      // number
+let state=false // boolean
+
+// bigInt
+// null => stand alone
+// undefined
+// symbol => uniqueness
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
