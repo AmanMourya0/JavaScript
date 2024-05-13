819de07820fdaa8e52190005b10be1c17977ba1d
AmanMourya0 <118892102+AmanMourya0@users.noreply.github.com>
2024-05-13 21:48:16 +0000

    type conversions

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+let score="33";
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueStringToInt=Number(score)
+// console.log(typeof(valueStringToInt),valueStringToInt);
+
+//"33" => 33
+// "33ab" => NaN
+// "qwerty" => NaN
+
+let isLoggin=1
+let valueToBoolean =Boolean(isLoggin)
+// console.log(valueToBoolean);
+
+// "" ,NaN,null,0,undefined
+let stringNum=99
+let valueToString=String(stringNum)
+console.log(typeof(valueToString));
