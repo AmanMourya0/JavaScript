7764c2237fecce42833ee7d6eb773e10a5482295
AmanMourya0 <118892102+AmanMourya0@users.noreply.github.com>
2024-05-14 06:17:40 +0000

    ++ operatorion

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -15,4 +15,17 @@ let valueToBoolean =Boolean(isLoggin)
 // "" ,NaN,null,0,undefined
 let stringNum=99
 let valueToString=String(stringNum)
-console.log(typeof(valueToString));
+// console.log(typeof(valueToString));
+
+// console.log(+"34"); // + operator converts string to number data types
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
