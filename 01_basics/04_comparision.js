2bb45835aa48be58b47b2febd0c4bcb6a40207dd
AmanMourya0 <118892102+AmanMourya0@users.noreply.github.com>
2024-05-14 06:40:19 +0000

    unexpected output of comparision operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+// console.log('2'>1);
+// console.log('2'>1);
+
+// comparision and equality check in js are different
+// these can lead unexpected output
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// === (strict check)
